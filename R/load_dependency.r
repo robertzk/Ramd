c7e014743fcc9a045b046ca5a0de152fbbb3a844
Robert Krzyzanowski <technoguyrob@gmail.com>
2013-10-24 19:00:39 -0500

    braindead simple caching mechanism

--- R/load_dependency.r
+++ R/load_dependency.r
@@ -4,7 +4,19 @@
 #' \dontrun{
 #'   helper <- load_dependency('path/to/helper')
 #' }
+cache <- list()
 load_dependency <- function(dep) {
   path <- base::normalizePath(paste(current_directory(), "/", dep, '.r', sep = ''))
-  invisible(source(path)$value)
+  fileinfo <- file.info(path)
+  mtime <- fileinfo$mtime
+
+  value <- NULL
+  if (path %in% names(cache) && mtime == cache[[path]]$mtime)
+    value <- cache[[path]]$value
+  else {
+    print('setting cache') # TODO: Check if file updated
+    value <- source(path)$value
+    cache[[path]] <- list(value = value, mtime = mtime)
+  }
+  invisible(value)
 }
