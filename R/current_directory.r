885a259c20fb1ae1e1ed73c80b5e0f600147f09c
RobertZK <technoguyrob@gmail.com>
2013-10-30 23:18:03 -0500

    changes to current directory function

--- R/current_directory.r
+++ R/current_directory.r
@@ -3,6 +3,10 @@ current_directory <-
   (function(frames = sys.frames()) {
     # http://stackoverflow.com/questions/1815606/rscript-determine-path-of-the-executing-script
     frame_files <- Filter(Negate(is.null), lapply(frames, function(x) x$ofile))
+    if (length(frame_files) == 0)
+      stop("Ramd cannot identify what directory you are currently executing ",
+           "in. Please specify it using base::setwd. Note that this is ",
+           "especially necessary from the console.")
     dirname(frame_files[[length(frame_files)]])
   })
 
