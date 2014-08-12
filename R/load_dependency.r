73327fd7589a45d8f0289a7ccf33e7dab76407ad
RobertZK <technoguyrob@gmail.com>
2014-08-11 19:35:41 -0500

    fix load_dependency

--- R/load_dependency.r
+++ R/load_dependency.r
@@ -5,9 +5,12 @@
 #'   helper <- load_dependency('path/to/helper')
 #' }
 load_dependency <- function(dep) {
-  path <- suppressWarnings(base::normalizePath(
-           paste(current_directory(), "/", dep, sep = '')))
-  if (!file.exists(path)) path <- paste(path, '.r', sep = '')
+  path <- suppressWarnings(base::normalizePath(file.path(current_directory(), dep)))
+  if (!file.exists(path)) {
+    new_path <- paste(path, '.r', sep = '')
+    if (!file.exists(new_path)) new_path <- paste(path, '.R', sep = '')
+    path <- new_path
+  }
   if (!file.exists(path))
     stop(paste("Unable to load dependency '", dep, "'", sep = ''))
   fileinfo <- file.info(path)
