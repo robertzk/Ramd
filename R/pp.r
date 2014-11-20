406bc3be52979beef508fa9da4eef3553b57ebee
RobertZK <technoguyrob@gmail.com>
2014-11-20 16:32:37 -0600

    add pp documentation per CRAN reviewer

--- R/pp.r
+++ R/pp.r
@@ -1,5 +1,29 @@
-# String interpolation in R!
-#' export
+#' Ruby-like string interpolation
+#'
+#' To interpolate any R object into a string, it can be wrapped in \code{#\{...\}}
+#' and within a string. This is equivalent to evaluating whatever is in this block
+#' within the current environment (see \code{base::environment} when called with
+#' no arguments).
+#'
+#' @param ... arbitrary many character vectors, that will be concatenated
+#'   using the \code{sep} parameter as delimiter. Any non-length 1 vectors
+#'   will also be collapsed using the \code{collapse} parameter.
+#' @param envir environment. Where to evaluate any interpolated strings,
+#'   by default \code{parent.frame()}, the current local environment.
+#' @param sep character. The delimiter to use to concatenate passed
+#'   strings, by default the empty string \code{''}.
+#' @param collapse character. The delimiter to use to concatenate passed
+#'   character vectors of length greater than 1, by default the empty string
+#'  \code{''}.
+#' @export
+#' @examples
+#' x <- 1; y <- 2
+#' cat(pp("1 + #{x} = #{1 + x}")) # "1 + 1 = 2"
+#' cat(pp(1:3, " are numbers")) # "123 are numbers"
+#' cat(pp(1:3, " are numbers", collapse = " and "))
+#' # "1 and 2 and 3 are numbers"
+#' cat(pp("x = #{x}", "y = #{y}", "x + y = #{x + y}", sep = ", "))
+#' # x = 1, y = 2, x + y = 3
 pp <- function(..., envir = parent.frame(), sep = '', collapse = '') {
   string <- list(...)
   if (length(string) > 1)
