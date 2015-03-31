dd75be0792426eabe3d8b18fb6e0be0309bfa346
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-03-31 17:56:53 -0500

    suppressPackageStartupMessages

--- R/packages.r
+++ R/packages.r
@@ -11,6 +11,6 @@
 packages <- function(...) {
   split_packages <- function(string) strsplit(string, '[^a-zA-Z.$0-9_]+')[[1]]
   pkgs <- unlist(lapply(unlist(list(...)), split_packages))
-  sapply(pkgs, load_package)
+  suppressPackageStartupMessages(sapply(pkgs, load_package))
   TRUE
 }
